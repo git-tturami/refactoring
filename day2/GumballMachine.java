b329b75d82c53eb77e1ff1b6822fe785d3eae503
Hyunsik Boo <bhs9194@nate.com>
2019-05-07 17:02:47 +0900

    Create GumballMachine.java

---
+++ day2/GumballMachine.java
@@ -0,0 +1,160 @@
+class GumballMachine {
+    State soldOutState;
+    State noQuarterState;
+    State hasQuarterState;
+    State soldState;
+
+    State state = soldOutState;
+    int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState =  new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void releaseBall() {
+        if (count != 0) {
+            count--;
+        }
+    }
+}
+
+public class SoldState implements State {
+    private GumballMachine machine;
+
+    public SoldState(GumballMachine machine) {
+        this.machine = machine;
+    }
+
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+
+    }
+}
+
+public class SoldOutState implements State {
+    private GumballMachine machine;
+
+    public SoldOutState(GumballMachine machine) {
+        this.machine = machine;
+    }
+
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+        
+    }
+}
+
+public class NoQuarterState implements State {
+    private GumballMachine machine;
+
+    public NoQuarterState(GumballMachine machine) {
+        this.machine = machine;
+    }
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+        
+    }
+}
+
+public class HasQuarterState implements State {
+    private GumballMachine machine;
+
+    public HasQuarterState(GumballMachine machine) {
+        this.machine = machine;
+    }
+
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+        
+    }
+}
+
+public interface State {
+    void insertQuarter();
+    void ejectQuarter();
+    void turnCrank();
+    void dispense();
+}
